b85139e3d5ab29284c37c53a1460df5a4b71ad8a
Afelipemelo <melosantosandres@gmail.com>
2022-11-14 22:44:44 -0500

    cambio

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -46,6 +46,7 @@ module.exports = function (config) {
     ],
     customLaunchers: {
       ChromeHeadless: {
+        
           base: 'Chrome',
           flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
       }
